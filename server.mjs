accdb88b11d64e54557220c2322f820ebe916b03
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-04-28 18:39:35 -0400

    checking post

--- server.mjs
+++ server.mjs
@@ -1,10 +1,10 @@
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
+const fs = require("fs");
 const express = require("express");
 const path = require("path");
-const fs = require("fs");
-const { v4: uuidv4 } = require("uuid");
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,35 +13,32 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/api/notes", (req, res) => {
-  const notes = JSON.parse(
-    fs.readFileSync(path.join(__dirname, "db/db.json"), "utf8")
-  );
-  res.json(notes);
-});
-
-app.post("/api/notes", (req, res) => {
-  const newNote = {
-    id: uuidv4(),
-    title: req.body.title,
-    text: req.body.text,
-  };
-
-  let notes = JSON.parse(
-    fs.readFileSync(path.join(__dirname, "db/db.json"), "utf8")
-  );
-  notes.push(newNote);
-
-  fs.writeFileSync(
-    path.join(__dirname, "db/db.json"),
-    JSON.stringify(notes, null, 2)
-  );
-
-  res.json(newNote);
+// Log each request to the console
+app.use((req, res, next) => {
+  console.log(`Received ${req.method} request for ${req.url}`);
+  next();
 });
 
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  const notes = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+  res.json(notes);
+});
+
+app.post("/api/notes", (req, res) => {
+  console.log(`Received POST request with data: ${JSON.stringify(req.body)}`);
+  const newNote = {
+    id: uuidv4(),
+    title: req.body.title,
+    text: req.body.text
+  };
+  let notes = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+  notes.push(newNote);
+  fs.writeFileSync("db/db.json", JSON.stringify(notes));
+  res.json(newNote);
 });
 
 app.get("*", (req, res) => {
