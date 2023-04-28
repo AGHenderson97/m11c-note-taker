fcf41aa54af250c246e87b6b37d0c675dfec3dfe
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-04-28 18:23:00 -0400

    updated code for functionality

--- server.mjs
+++ server.mjs
@@ -1,27 +1,25 @@
-import express from "express";
-import path from "path";
-import fs from "fs";
-import { v4 as uuidv4 } from "uuid";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Middleware for handling POST data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Serve static assets
 app.use(express.static("public"));
 
-// API routes
-
-// GET all notes
 app.get("/api/notes", (req, res) => {
-  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, "db/db.json"), "utf8"));
+  const notes = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "db/db.json"), "utf8")
+  );
   res.json(notes);
 });
 
-// POST a new note
 app.post("/api/notes", (req, res) => {
   const newNote = {
     id: uuidv4(),
@@ -29,27 +27,27 @@ app.post("/api/notes", (req, res) => {
     text: req.body.text,
   };
 
-  let notes = JSON.parse(fs.readFileSync(path.join(__dirname, "db/db.json"), "utf8"));
+  let notes = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "db/db.json"), "utf8")
+  );
   notes.push(newNote);
 
-  fs.writeFileSync(path.join(__dirname, "db/db.json"), JSON.stringify(notes));
+  fs.writeFileSync(
+    path.join(__dirname, "db/db.json"),
+    JSON.stringify(notes, null, 2)
+  );
 
   res.json(newNote);
 });
 
-// HTML routes
-
-// GET notes page
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-// GET home page
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-// Start the server
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`App listening on PORT ${PORT}`);
 });
