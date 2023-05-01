89f12aa4d0f514fdff1c91d3f47ff666173ea219
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-05-01 16:22:48 -0400

    Updated server.js to use correct port

--- server.mjs
+++ server.mjs
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 import { promises as fs } from "fs";
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5500; // Corrected port number
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
